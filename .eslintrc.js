7d2e7975a51c6ba07e4708159a077a8725eb95c0
James Ford <james.f@mmtdigital.co.uk>
2017-02-24 16:29:06 +0000

    Merge eslint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,43 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  "extends": ["eslint:recommended"],
+
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "experimentalObjectRestSpread": true
+    }
   },
-  env: {
-    browser: true,
+
+  "env": {
+    "browser": true,
+    "node": false,
+    "amd": true,
+    "es6": true,
     "mocha": true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
+
   "globals": {
-    "expect": true,
-    "sinon": true
+    "define": true
+  },
+
+  "rules": {
+    "strict": ["warn", "global"],
+    "no-unused-vars": ["warn", { "vars": "all", "args": "all", "caughtErrors": "none" }],
+    "no-console": ["warn", { "allow": ["warn", "error"] }],
+    "camelcase": ["warn", { "properties": "always" }],
+    "consistent-return": "warn",
+    "arrow-spacing": "warn",
+    "arrow-parens": ["warn", "always"],
+    "arrow-body-style": ["warn", "as-needed"],
+    "semi": ["warn", "always"],
+    "no-confusing-arrow": ["warn", { "allowParens": false }],
+    "no-constant-condition": "warn",
+    "no-labels": "warn",
+    "no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 1 }],
+    "func-style": "off",
   }
-}
+};
